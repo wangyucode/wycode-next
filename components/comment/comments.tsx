8f16d8741d1dbf127027913ba7dbb7a54a75ba77
yu.wang <wangyu@wycode.cn>
2022-05-26 15:16:09 +0800

    post comment

--- components/comment/comments.tsx
+++ components/comment/comments.tsx
@@ -2,6 +2,7 @@ import {useEffect, useState} from "react";
 import {useRouter} from "next/router";
 import {ReplyIcon, XIcon} from "@heroicons/react/outline";
 import Comment from "./comment";
+import BannerMsg from "../banner-msg";
 
 export const key = '114c03ec4d6f40a4a1490a5638d8141d';
 export const app = 'wycode'
@@ -10,9 +11,28 @@ export default function Comments() {
 
     const topic = useRouter().asPath.substring(1);
     const [replying, setReplying] = useState<any>(null);
-    const [comments, setComments] = useState([]);
+    const [comments, setComments] = useState<any[]>([]);
+    const [content, setContent] = useState('');
+    const [email, setEmail] = useState('');
+    const [error, setError] = useState('');
+    const [info, setInfo] = useState('');
+    const [loading, setLoading] = useState(false);
+
+    function onChangeContent(event: any) {
+        setError('');
+        setInfo('');
+        setContent(event.target.value);
+    }
+
+    function onChangeEmail(event: any) {
+        setError('');
+        setInfo('');
+        setEmail(event.target.value);
+    }
 
     useEffect(() => {
+        const storedEmail = localStorage.getItem('wycode.username');
+        if (storedEmail) setEmail(storedEmail);
         fetch(`https://wycode.cn/node/comments?a=${app}&k=${key}&t=${topic}`)
             .then((res) => res.json())
             .then(res => {
@@ -24,52 +44,53 @@ export default function Comments() {
     }, []);
 
     function handleReply() {
-        alert("正在施工...");
-        // if (!this.content) {
-        //     this.errorMsg = '内容不能为空';
-        //     return;
-        // }
-        // if (!/^\S+@\w+(\.[\w]+)+$/.test(this.username)) {
-        //     this.errorMsg = '请输入正确的Email地址。您的Email不会被公开。';
-        //     return;
-        // }
-        // this.loading = true;
-        // var comment = {
-        //     key: this.key,
-        //     app: this.app,
-        //     topic: this.path,
-        //     content: this.content,
-        //     fromUserName: this.username,
-        //     to: this.toComment ? {
-        //         user: this.toComment.user,
-        //         content: this.toComment.content
-        //     } : undefined,
-        //     like: 0,
-        //     type: 0
-        // };
-        // localStorage.setItem('wycode.username', this.username);
-        // fetch(server + '/node/comments', { method: 'POST', body: JSON.stringify(comment), headers: { 'Content-Type': 'application/json' } })
-        //     .then(res => res.json())
-        //     .then(res => {
-        //         console.log('newComment->', res);
-        //         this.loading = false;
-        //         if (res && res.success) {
-        //             this.content = '';
-        //             this.toComment = null;
-        //             comment._id = res.payload;
-        //             var username = comment.fromUserName.charAt(0);
-        //             var atIndex = comment.fromUserName.lastIndexOf('@');
-        //             username += new Array(atIndex).fill('*').join('');
-        //             username += comment.fromUserName.substring(atIndex);
-        //             comment.user = username;
-        //             comment.createTime = "刚刚";
-        //             this.comments.push(comment);
-        //             this.successMsg = '发布成功';
-        //             this.errorMsg = '';
-        //         } else {
-        //             this.errorMsg = '发布失败：' + res.message
-        //         }
-        //     });
+        if (!content) {
+            setInfo('');
+            setError('内容不能为空');
+            return;
+        }
+        if (!/^\S+@\w+(\.[\w]+)+$/.test(email)) {
+            setInfo('');
+            setError('请输入正确的Email地址。您的Email不会被公开。');
+            return;
+        }
+        localStorage.setItem('wycode.username', email);
+        setLoading(true);
+        const comment: any = {
+            key,
+            app,
+            topic,
+            content,
+            fromUserName: email,
+            to: replying ? {
+                user: replying.user,
+                content: replying.content
+            } : null,
+            like: 0,
+            type: 0
+        };
+        fetch('https://wycode.cn/node/comments', { method: 'POST', body: JSON.stringify(comment), headers: { 'Content-Type': 'application/json' } })
+            .then(res => res.json())
+            .then(res => {
+                console.log('newComment->', res);
+                if (res && res.success) {
+                    setContent('');
+                    setReplying(null);
+                    comment._id = res.payload;
+                    let username = comment.fromUserName.charAt(0);
+                    const atIndex = comment.fromUserName.lastIndexOf('@');
+                    username += new Array(atIndex).fill('*').join('');
+                    username += comment.fromUserName.substring(atIndex);
+                    comment.user = username;
+                    comment.createTime = new Date();
+                    setComments([...comments, comment])
+                    setInfo('发布成功');
+                } else {
+                    setError(`发布失败：${res.message}`);
+                }
+            })
+            .catch(setError)
+            .finally(()=>setLoading(false));
     }
 
     return (<div className="flex flex-col border-t w-full border-slate-700/30 dark:border-slate-300/30 mt-8 p-4">
@@ -82,14 +103,22 @@ export default function Comments() {
                 <span className="flex-1">{replying.content}</span>
                 <button onClick={() => setReplying(null)}><XIcon className="w-4 inline"/></button>
             </p>}
-        <textarea placeholder="评论内容..." rows={3}
+        {error && <BannerMsg type="error" msg={error}/>}
+        {info && <BannerMsg msg={info}/>}
+        <textarea placeholder="评论内容..."
+                  rows={3}
+                  onChange={onChangeContent}
+                  value={content}
                   className="mt-2 py-2 px-4 border bg-slate-500/5 rounded border-slate-700/30 dark:border-slate-300/30"></textarea>
         <div className="flex mt-2">
             <input placeholder="邮箱（不会被公开）"
+                   value={email}
+                   onChange={onChangeEmail}
                    className="flex-1 px-4 py-2 bg-slate-500/5 border rounded-l focus-visible:outline-0 focus-visible:ring-2 border-slate-700/30 dark:border-slate-300/30"/>
             <button
                 onClick={handleReply}
-                className="flex items-center px-4 py-2 bg-sky-600 hover:bg-sky-700 rounded-r text-slate-100 active:ring-2 border-slate-700/30 dark:border-slate-300/30">
+                disabled={loading}
+                className="flex items-center px-4 py-2 bg-sky-600 disabled:bg-slate-500 hover:bg-sky-700 rounded-r text-slate-100 active:ring-2 disabled:active:ring-0 border-slate-700/30 dark:border-slate-300/30">
                 <ReplyIcon className="w-4 inline mr-1"/>回复
             </button>
         </div>

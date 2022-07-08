2578aeba24354cca4e7ed1f4e064ca1177aa1e85
yu.wang <wangyu@wycode.cn>
2022-07-08 17:39:03 +0800

    only show search on blog pages

--- pages/page/[page].tsx
+++ pages/page/[page].tsx
@@ -7,7 +7,7 @@ import Pager from "../../components/pager";
 export default function Page({posts, page,total}: { posts: Post[], page: number, total: number }) {
     return (
         <Layout>
-            <div className="px-4 flex flex-col lg:flex-row max-w-7xl mx-auto">
+            <div className="px-4 flex flex-col max-w-4xl mx-auto">
                 <ul className="min-w-0">
                     {posts.map(post => (
                         <li className="border-b last:border-0 border-slate-700/30 dark:border-slate-300/30 py-4 max-w-full"

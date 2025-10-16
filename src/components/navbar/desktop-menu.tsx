b0b5617eb18b83d22e4b4d19a54931c35f27acc2
yu.wang <wangyu@wycode.cn>
2025-10-17 03:03:30 +0800

    feat(导航栏): 添加剪切板页面链接和功能
    添加剪切板页面路由和导航栏菜单项，实现跨平台剪切板功能

--- src/components/navbar/desktop-menu.tsx
+++ src/components/navbar/desktop-menu.tsx
@@ -1,6 +1,8 @@
 'use client'
+import { ClipboardIcon, PencilIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { MenuLinks } from "./navbar";
 
 export default function DesktopMenu() {
     const pathname = usePathname();
@@ -8,7 +10,8 @@ export default function DesktopMenu() {
 
     return (
         <ul className="menu menu-horizontal p-1 gap-1">
-            <li ><Link href="/" className={`font-bold ${isBlogPage ? "menu-active" : ""}`}>博客</Link></li>
+            <li ><Link href={MenuLinks.HOME} className={`font-bold ${isBlogPage ? "menu-active" : ""}`}><PencilIcon className="w-4" />博客</Link></li>
+            <li ><Link href={MenuLinks.CLIPBOARD} className={`font-bold ${pathname === MenuLinks.CLIPBOARD ? "menu-active" : ""}`}><ClipboardIcon className="w-4" />剪切板</Link></li>
             <li>
                 <details>
                     <summary>更多</summary>
--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -7,12 +7,25 @@ import MobileMenu from "./mobile-menu";
 import DesktopMenu from "./desktop-menu";
 import SearchButton from "./search-button";
 
+export enum MenuLinks {
+    HOME = '/',
+    APP = '/apps',
+    CLIPBOARD = '/clipboard',
+    DASHBOARD = '/lab/dashboard',
+    DOGGER = '/lab/dogger',
+    CHAT = '/lab/chat',
+    SWAGGER = '/lab/swagger-ui',
+    MONGO = '/lab/mongo',
+    SKILLS = '/skills',
+    VENDING = '/lab/vending',
+}
+
 export default function Navbar() {
 
     return (
         <header className="navbar shadow-sm bg-base-200/60 backdrop-blur fixed z-10">
             <div className="navbar-start">
-                <Link href="/" className="text-xl font-bold hover:text-sky-600 flex gap-1 items-center px-2">
+                <Link href={MenuLinks.HOME} className="text-xl font-bold hover:text-sky-600 flex gap-1 items-center px-2">
                     <Image src={icon} alt="王郁的小站" width={32} height={32} />
                     <span className="bg-gradient-to-br dark:bg-gradient-to-tr from-sky-600 to-pink-600 dark:from-pink-400 dark:to-sky-400 bg-clip-text text-transparent">王郁的小站</span>
                 </Link>

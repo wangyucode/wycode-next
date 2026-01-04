513e0d21c631595fba30cb4e65e35baa56c682a3
yu.wang <wangyu@wycode.cn>
2026-01-04 22:39:39 +0800

    ci(workflow): 更新部署令牌并修复构建配置

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     output: "export",
+    generateBuildId: () => {
+        return "wycode_has_super_power";
+    },
 };
 
 export default nextConfig;

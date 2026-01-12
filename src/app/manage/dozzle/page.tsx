0802502396c31d326fd824fd4c5972e9993f8856
yu.wang <wangyu@wycode.cn>
2026-01-12 22:26:31 +0800

    feat(管理): 添加Dozzle日志管理页面并升级Swagger UI
    - 新增Dozzle日志管理页面及导航菜单项
    - 将Swagger页面从iframe改为使用swagger-ui-react组件
    - 更新next-env.d.ts文件路径
    - 升级项目版本至3.4.0并添加swagger-ui-react依赖

---
+++ src/app/manage/dozzle/page.tsx
@@ -0,0 +1,3 @@
+export default function DozzlePage() {
+    return <iframe className="flex-1 card container shadow mx-auto md:my-4" src="https://wycode.cn/dozzle/" />;
+}
--- src/app/manage/swagger/page.tsx
+++ src/app/manage/swagger/page.tsx
@@ -1,3 +1,10 @@
+import SwaggerUI from "swagger-ui-react"
+import "swagger-ui-react/swagger-ui.css"
+
 export default function SwaggerPage() {
-    return <iframe className="flex-1 card container shadow mx-auto md:my-4" src="https://wycode.cn/api/v1/doc/" />;
+    return (
+        <div className="flex-1 card container shadow mx-auto md:my-4 bg-base-100">
+            <SwaggerUI url="https://wycode.cn/api/v1/openapi.yml" />
+        </div>
+    );
 }
